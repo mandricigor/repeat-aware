2cb2cd857e951a5fd33095d81c7e3395d14e41a0
Igor Mandric <mandric.igor@gmail.com>
2017-12-31 13:21:19 -0500

    updated procedures - second revision

--- build_out_scaf.py
+++ build_out_scaf.py
@@ -1,169 +1,84 @@
 
-from collections import Counter
+
+
 from Bio import SeqIO
-from Bio.Seq import Seq
+import re
 import sys
-import shutil
-from fastaq import sequences, utils, intervals, tasks
+from collections import Counter
 
-
-
-prefix = sys.argv[3]
-similarity_level = sys.argv[4]
-smallest_contig_length = sys.argv[5]
-
-utils.syscall(' '.join(['nucmer', '-p', prefix, sys.argv[1], sys.argv[2], '--maxmatch']))
-utils.syscall(' '.join(['delta-filter', '-i %s -l %s -r' % (similarity_level, smallest_contig_length), prefix + ".delta", '>', prefix + ".filter"]))
-utils.syscall(' '.join(['show-coords', '-dTlro', prefix + ".filter", '>', prefix + ".coords"]))
-
-
-output_scaffolds = {}
-for record in SeqIO.parse(sys.argv[1], "fasta"):
-    output_scaffolds[record.id] = str(record.seq)
-
-ref_contigs = {}
+contigs = {}
 for record in SeqIO.parse(sys.argv[2], "fasta"):
-    ref_contigs[record.id] = str(record.seq)
+    contigs[str(record.seq).upper()] = record.id + ":::1"
+    contigs[str(record.seq.reverse_complement()).upper()] = record.id + ":::-1"
+
+a = Counter(contigs.values())
 
 
-#prefix = "prefix"
 
-
-
-with open(prefix + ".coords") as f:
-    a = f.readlines()
-
-a = a[4:] # drop the header lines from nucmer hit file
-a = list(map(lambda x: x.strip().split(), a))
-
-
-contig_hits = [x[12] for x in a]
-contig_hits_counts = Counter(contig_hits)
-
-# groups is the list of groups by reference
-groups = [[a[0]]]
-linegroups = [[0]]
-for i in range(1, len(a)):
-    if a[i][11] == groups[-1][-1][11]:
-        groups[-1].append(a[i])
-        linegroups[-1].append(i)
-    else:
-        groups.append([a[i]])
-        linegroups.append([i])
-
-
-# because we may have that some contigs 
-allgroups = []
-alllinegroups = []
-for linegroup, group in zip(linegroups, groups):
-    group2 = [[group[0]]]
-    linegroup2 = [[linegroup[0]]]
-    for j in range(1, len(group)):
-        if group[j][12] == group2[-1][-1][12]:
-            group2[-1].append(group[j])
-            linegroup2[-1].append(linegroup[j])
-        else:
-            group2.append([group[j]])
-            linegroup2.append([linegroup[j]])
-    allgroups.append(group2)
-    alllinegroups.append(linegroup2)
-
-
-contig_map = {}
-used_contigs = set()
-
-count = 0
 scaffolds = []
-chosen_lines = []
-for alllinegroup, group in zip(alllinegroups, allgroups):
-    scaf = []
-    chosen = []
-    for linegroup2, group2 in zip(alllinegroup, group):
-        if len(group2) == 1 and len(group2[0]) == 13:
-            # if the contig does not match completely -> skip it
-            # we do not need it anymore
-            name = group2[0][12]
-            if int(group2[0][1]) - int(group2[0][0]) > 0.8 * int(group2[0][8]):
-                scaf.append(name + ":::" + group2[0][10] + ":::" + str(int(group2[0][1]) - int(group2[0][0])))
-                chosen.append(linegroup2)
-                used_contigs.add(group2[0][12])
-            #print(aaa)
-            #print ("CONTINUE")
-            #continue
-        elif len(group2) == 1 and len(group2[0]) == 14 and group2[0][-1] != "[CONTAINED]":
-            # it has probably a full match
-                name = group2[0][12]
-                scaf.append(name + ":::" + group2[0][10] + ":::" + str(int(group2[0][1]) - int(group2[0][0])))
-                chosen.append(linegroup2)
-                used_contigs.add(group2[0][12])
-        else:
-            # here we may have contigs corresponding to circular genomes
-            # if it has both [BEGINS] and [ENDS] -> we just split it
-            # or we still have mis-assembly - just merge all hits
-            if int(group2[-1][1]) - int(group2[0][0]) > 0.8 * int(group2[0][8]):
-                name = group2[0][12]
-                scaf.append(name + ":::" + group2[0][10] + ":::" + str(int(group2[-1][1]) - int(group2[0][0])))
-                chosen.append(linegroup2)
-                used_contigs.add(group2[0][12])
+for scaffold in SeqIO.parse(sys.argv[1], "fasta"):
+    scaffolds.append(scaffold.seq)
+
+
+scafs = []
+for scaffold in scaffolds:
+    myscaf = []
+    metacontigs = re.split("[N|n]+", str(scaffold))
+    distances = iter(map(len, re.findall("[N|n]+", str(scaffold))) + [0])
+    for jj, meta in enumerate(metacontigs):
+        if jj % 1000 == 0:
+            print jj
+        print jj, "MMM"
+        contig = contigs.get(meta, "NA")
+        if contig == "NA":
+            contained = False
+            good = None
+            for key in contigs.keys():
+                if meta.upper() in key:
+                    contained = True
+                    good = contigs[key]
+                    break
+            if contained:
+                contig = good + ":::" + str(len(meta)) + ":::" + str(distances.next())
+                myscaf.append(contig)
             else:
-                # check if this is the only hit
-                #print ("DECI SUKA")
-                name = group2[0][12]
-                if group2[0][12] in contig_hits_counts and contig_hits_counts[group2[0][12]] == 1:
-                    scaf.append(name + ":::" + group2[0][10] + ":::" + str(int(group2[-1][1]) - int(group2[0][0])))
-                    used_contigs.add(group2[0][12])
-                #chosen.append(linegroup2)
-        count += 1
-    if scaf:
-        chosen_lines.append(chosen)
-        scaffolds.append(scaf)
+                # scaffold consists of several contigs sticked together
+                intervals = {}
+                for key in contigs.keys():
+                    position = meta.upper().find(key)
+                    if position != -1:
+                        intervals[contigs[key] + ":::" + str(len(key))] = (position, position + len(key))
+                intervals = sorted(intervals.items(), key=lambda x: x[1][0])
+                remove = set()
+                for i in range(len(intervals)):
+                    for j in range(len(intervals)):
+                        if i == j:
+                            continue
+                        int1 = intervals[i][1]
+                        int2 = intervals[j][1]
+                        if int1[0] >= int2[0] and int1[1] <= int2[1]:
+                            # interval int1 is containing in another one - skip it
+                            remove.add(i)
+                intervals2 = []
+                for i in range(len(intervals)):
+                    if i not in remove:
+                        intervals2.append(intervals[i])
+                mymyscaf = []
+                for i in range(len(intervals2) - 1):
+                    mymyscaf.append(intervals2[i][0] + ":::" + str(intervals2[i + 1][1][0] - intervals2[i][1][1]))
+                mymyscaf.append(intervals2[-1][0] + ":::" + str(distances.next()))
+                myscaf.extend(mymyscaf)
+        else:
+            contig = contig + ":::" + str(len(meta)) + ":::" + str(distances.next())
+            myscaf.append(contig)
+    scafs.append(myscaf)
 
 
-#print("COUNT:" + str(count))
-
-unused_contigs = set(ref_contigs.keys()) - used_contigs
-
-
-for refcont in unused_contigs:
-    for outscaf, outseq in output_scaffolds.items():
-        if outseq in ref_contigs[refcont]:
-            #print(refcont + " " +  outscaf)
-            break
-        elif outseq in str(Seq(ref_contigs[refcont]).reverse_complement()):
-            #print (refcont + " " + outscaf)
-            break
-
-
-distances = []
-for cluster in chosen_lines:
-    dist = [0]
-    for group_number in range(len(cluster) - 2, -1, -1):
-        dist.insert(0, int(a[cluster[group_number + 1][0]][0]) - int(a[cluster[group_number][-1]][1]))
-    distances.append(dist)
-
-
-rev_contig_map = {}
-for x, y in contig_map.items():
-    rev_contig_map[y] = x
-
-
-with open(prefix + ".scaf", "w") as f:
-    counter = 1
-    for dist, scaffold in zip(distances, scaffolds):
-        f.write(">scaffold_%s\n" % counter)
-        for d, scaf in zip(dist, scaffold):
-            f.write("%s:::%s\n" % (scaf, d))
-        counter += 1
-    for contig in unused_contigs:
-        f.write(">scaffold_%s\n" % counter)
-        f.write("%s:::1:::%s:::0\n" % (contig, len(ref_contigs[contig])))
-        counter += 1
-
-
-
-
-
-
+with open(sys.argv[3], "w") as f:
+    for i, scaf in enumerate(scafs):
+        f.write(">scaffold_%s\n" % i)
+        for c in scaf:
+            f.write("%s\n" % c)
 
 
 
--- validation.py
+++ validation.py
@@ -4,6 +4,7 @@
 import sys
 import logging
 #import numpy as np
+import time
 import cplex
 import math
 #import networkx as nx
@@ -273,6 +274,8 @@ for con in cons2:
         names = ['pair']\
     )
 
+#print ref_distances
+#print scaf_distances
 
 
 allzeds = []
@@ -283,24 +286,31 @@ xz_corr = {} # correspondence between x variables and z
 used_x = set()
 z = 0
 for link in cons_pairs_outer:
+    #print link, "THIS IS LINK"
     conses = cons_pairs_outer[link]
     conses_orient = cons_pairs_orient[link]
     cind = outer_indices[link]
     cind_orient = outer_orient[link]
-    zeds = []
+    #zeds = []
     for cons, cori in zip(conses, conses_orient):
+        #print cons, cori, "CONS CORI"
         zz = []
         ww = []
         for index, sori in zip(cind, cind_orient):
             rgap = max(0, ref_distances[tuple(sorted([cons[0][index[0]][0], cons[1][index[1]][0]]))])
             sgap = max(0, scaf_distances[tuple(sorted([cons[0][index[0]][1], cons[1][index[1]][1]], key=lambda mm: int(mm.split("_")[1])))])
+            #print sgap, rgap, cons, cind, index
             if abs(rgap - sgap) > distance_threshold:
-                wrong_distance_links.add(tuple(sorted([int(cons[0][index[0]][1].split("_")[1]), int(cons[1][index[1]][1].split("_")[1])])))
+                #print "ADDING WRONG", tuple(sorted([int(cons[0][index[0]][0]), int(cons[1][index[1]][0])]))
+                #wrong_distance_links.add(tuple(sorted([int(cons[0][index[0]][1].split("_")[1]), int(cons[1][index[1]][1].split("_")[1])])))
+                wrong_distance_links.add(tuple(sorted([int(cons[0][index[0]][0]), int(cons[1][index[1]][0])])))
             elif cori == sori and abs(rgap - sgap) <= distance_threshold:
+                #print "ADDING CORRECT"
                 w = 1
                 ww.append(w)
                 cpx.variables.add(lb = [0], ub = [1], types = ["B"], names = ["Z#%s" % z])
                 inds = ["X#%s#%s" % cons[0][index[0]], "X#%s#%s" % cons[1][index[1]], "Z#%s" % z]
+                #print inds
                 used_x.add("X#%s#%s" % cons[0][index[0]])
                 used_x.add("X#%s#%s" % cons[1][index[1]])
                 xz_corr["Z#%s" % z] = ("X#%s#%s" % cons[0][index[0]], "X#%s#%s" % cons[1][index[1]])
@@ -312,12 +322,31 @@ for link in cons_pairs_outer:
                     rhs = [0],\
                     names = ['pair']\
                 )
-                zz.append("Z#%s" % z)
+                zz.append(["Z#%s" % z])
                 z += 1
+            #else:
+                #print cori, sori, "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+        for zzz in zz:
+            allzeds.extend(zzz)    
+        zedweights.extend(ww)
+        #print zz, "THIS  IS ZZ"
+        if len(zz) > 1:
+            for zzz in zip(*zz):
+                #print zzz, "JJJJJJJJJJJJJJJJJJJJJJJJJJ"
+                c = cplex.SparsePair(ind = zzz, val = [1] * len(zz))
+                cpx.linear_constraints.add( \
+                    lin_expr = [c],\
+                    senses = ["L"],\
+                    rhs = [1],\
+                    names = ['pair']\
+                )
+
+"""
         zeds.append(zz)
         zedweights.extend(ww)
     for zzz in zeds:
         allzeds.extend(zzz)
+    print zeds, "ZEDS"
     if len(zeds) > 1:
         for zzz in zip(*zeds):
             c = cplex.SparsePair(ind = zzz, val = [1] * len(zeds))
@@ -327,8 +356,9 @@ for link in cons_pairs_outer:
                 rhs = [1],\
                 names = ['pair']\
             )
+"""
 
-
+#print allzeds, "ALLZEDS"
 
 
 for zed, w in zip(allzeds, zedweights):
@@ -339,9 +369,12 @@ for zed, w in zip(allzeds, zedweights):
 cpx.objective.set_sense(cpx.objective.sense.maximize)
 cpx.set_problem_type(cpx.problem_type.MILP)
 cpx.write("program.txt", filetype="lp")
+start_time = cpx.get_time()
 cpx.solve()
+elapsed = cpx.get_time() - start_time
 
 answer_dict = {}
+
 for x in cpx.variables.get_names():
     if "X" in x:
         x = x.split("#")[1]
@@ -349,6 +382,9 @@ for x in cpx.variables.get_names():
 
 
 # --------------------------- processing ------------------
+
+#for x, y in zip(cpx.variables.get_names(), cpx.solution.get_values()):
+#    print x, y, "JJJJJJJJJJJJJ"
 
 scafs = []
 for scaf in scaffold:
@@ -364,6 +400,8 @@ for x, y in zip(cpx.variables.get_names(), cpx.solution.get_values()):
 
 good_links = []
 for x, y in zip(cpx.variables.get_names(), cpx.solution.get_values()):
+    #if "Z" in x:
+    #    print x, y, "MMM"
     if y > 0.5 and "Z" in x:
         # collect correct links
         fl, sl = xz_corr[x]
@@ -371,6 +409,7 @@ for x, y in zip(cpx.variables.get_names(), cpx.solution.get_values()):
         b1 = int(fl[2].split("_")[1])
         sl = sl.split("#")
         b2 = int(sl[2].split("_")[1])
+        #print b1, b2, "GOOD LINK"
         correct = tuple(sorted([b1, b2]))
         good_links.append(correct)
 
@@ -416,6 +455,8 @@ for x, y in newly_assigned.items():
     scaf_answer[u][v] = x
 
 
+#print scaf_answer
+
 # after assignment, if there is still something 
 scaf_answer_human = []
 asc = iter(allscaf)
@@ -425,9 +466,11 @@ for scaf in scaf_answer:
         myscaf.append(asc.next())
     scaf_answer_human.append(myscaf)
 
+#print scaf_answer_human
+#print len(scaf_answer_human)
+#print map(len, scaf_answer_human)
 
-
-
+print good_links
 wrong_links = []
 corrected_chains = []
 position = 0
@@ -435,6 +478,7 @@ for scaffold in scaf_answer:
     correct_subscaffold = [[scaffold[0]]]
     for j in range(1, len(scaffold)):
         if (position, position + 1) in good_links:
+            print scaffold[j - 1], scaffold[j], allscaf[position], allscaf[position + 1]
             correct_subscaffold[-1].append(scaffold[j])
         else:
             correct_subscaffold.append([scaffold[j]])
@@ -443,19 +487,90 @@ for scaffold in scaf_answer:
     position += 1
     corrected_chains.extend(correct_subscaffold)
 
-corrected_chains = [x for x in corrected_chains if x != ['NONE']]
+#print corrected_chains, "CORRECTED CHAINS"
+
+# compute correct links
+#correct_links = []
+#for chain in corrected_chains:
+#    if len(chain) > 1:
+#        for i in range(1, len(chain)):
+#            correct_links.append((allref[chain[i - 1]], allref[chain[i]]))
+        
+#for u, v in correct_links:
+#    print u, v
+
+#print wrong_links, "WRONG LINKS"
+
+#print corrected_chains
 
 
 scaf_answer_human = []
+scaf_answer_nonhuman = []
 asc = iter(allscaf)
+asc2 = iter(range(len(allscaf)))
 for scaf in corrected_chains:
     myscaf = []
+    myscaf2 = []
     for sc in scaf:
         myscaf.append(asc.next())
+        myscaf2.append(asc2.next())
     scaf_answer_human.append(myscaf)
+    scaf_answer_nonhuman.append(myscaf2)
+
+
+scaf_answer_nonhuman2 = []
+for uuu, vvv in zip(scaf_answer_nonhuman, corrected_chains):
+    if vvv != ['NONE']:
+        scaf_answer_nonhuman2.append(uuu)
+
+corrected_chains = [x for x in corrected_chains if x != ['NONE']]
+
+#print corrected_chains
+#print scaf_answer_nonhuman2, "NONHUMAN"
+
+# ---------------- code for computing corrected n50
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+lines = map(lambda x: x.strip(), lines)
+lines = [x for x in lines if ">" not in x]
+
+corlines = []
+for cchain, vvv in zip(scaf_answer_nonhuman2, corrected_chains):
+    corchain = []
+    for number in cchain:
+        corchain.append(lines[number])
+    #if vvv[0] > vvv[-1]:
+    #    corlines.append(list(reversed(corchain)))
+    #else:
+    corlines.append(corchain)
+
+
+corlens = []
+for cline in corlines:
+    l1 = map(lambda x: int(x.split(":::")[2]), cline)
+    l2 = map(lambda x: int(x.split(":::")[3]), cline)
+    corlens.append(sum(l1) + sum(l2[:-1]))
+
+
+sumalllens = sum(corlens) * 1.0 / 2
+scorlens = sorted(corlens, reverse=True)
+cursum = 0
+i = 0
+while (cursum < sumalllens):
+    cursum += scorlens[i]
+    i += 1
+
+#print "CORRECTED N50:", scorlens[i - 1]
+corn50 = scorlens[i - 1]
 
 
 
+
+# -------------------------------------------------
+
+
+#print scaf_answer_human
 
 # classify wrong links
 wrong_copy = 0
@@ -466,33 +581,53 @@ jumping_wrong_ord_ori = 0
 wrong_distance = 0
 
 
+#print wrong_distance_links
 
 for aa, bb, cc, dd in wrong_links:
+
+    print aa, bb, cc, dd, allscaf[aa], allscaf[bb] # enumerate all wrong links
     is_jumping = False
     is_wrong_ord_ori = False
     is_wrong_copy = False
     is_wrong_ref = False
 
-    if (aa, bb) in wrong_distance_links:
-        wrong_distance += 1
-        continue
+
     if cc != "NONE" and dd != "NONE":
         o1, o2, o3, o4 = sorient[int(aa)], sorient[int(bb)], rorient[int(cc)], rorient[int(dd)]
-        is_jumping = abs(int(cc) - int(dd)) > 1
-        if (o1, o2 != o3, o4) or (o1, o2 != op(o4), op(o3)):
-            is_wrong_ord_ori = True
+
+        is_wrong_ref = (refdict[allref[int(cc)]] & refdict[allref[int(dd)]] == set())
+        if is_wrong_ref:
+            print aa, bb, cc, dd, "WRONG_REF", refdict[allref[int(cc)]], refdict[allref[int(dd)]], refdict[allref[int(cc)]] & refdict[allref[int(dd)]], refdict[allref[int(cc)]] & refdict[allref[int(dd)]] == set(), is_wrong_ref, allscaf[aa], allscaf[bb]
+            wrong_ref += 1
+            #print aa, bb, cc, dd, "MMMMMMMMMMM", allscaf[aa], allscaf[bb]
+            continue
         is_wrong_copy = False
-        is_wrong_ref = refdict[allref[int(cc)]] & refdict[allref[int(dd)]] == set()
+        is_jumping = abs(int(cc) - int(dd)) > 1
+        #print is_jumping, allscaf[aa], allscaf[bb], "IS JUMPING"
+        #print aa < bb, cc < dd, (o1, o2 == o3, o4), (o1, o2 == op(o4), op(o3)), "KKK"
+        if not (((aa < bb) and (cc < dd) and ((o1, o2) == (o3, o4))) or ((((aa < bb) and (cc > dd)) or ((aa > bb) and (cc < dd))) and ((o1, o2) == (op(o3), op(o4))))):
+            is_wrong_ord_ori = True
+            print aa, bb, cc, dd, "WRONG ORDORI", allscaf[aa], allscaf[bb], o1, o2, o3, o4
+
+
+        #print aa, bb, cc, dd, is_wrong_ord_ori, tuple(sorted([cc, dd])) in wrong_distance_links, o1, o2, o3, o4
+        if tuple(sorted([cc, dd])) in wrong_distance_links and is_wrong_ord_ori == False:
+            wrong_distance += 1
+            continue
+
     else:
+        print aa, bb, cc, dd, "MMMMMMMMMMMMMMM", allscaf[aa], allscaf[bb], "WRONG COPY"
         is_jumping = False
         is_wrong_ord_ori = False
         is_wrong_ref = False
         is_wrong_copy = True
 
+    #print is_wrong_copy, is_wrong_ref, is_jumping, is_wrong_ord_ori, wrong_distance
+
     if is_wrong_copy:
         wrong_copy += 1
-    elif is_wrong_ref:
-        wrong_ref += 1
+    #elif is_wrong_ref:
+    #    wrong_ref += 1
     elif is_jumping and is_wrong_ord_ori:
         jumping_wrong_ord_ori += 1
     elif is_jumping:
@@ -500,18 +635,35 @@ for aa, bb, cc, dd in wrong_links:
     elif is_wrong_ord_ori:
         wrong_ord_ori += 1
 
-print "COPY:" , wrong_copy
-print "REF:", wrong_ref
-print "GAP:", wrong_distance
-print "JUMPING:", jumping
-print "ORDER_ORIENTATION:", wrong_ord_ori
-print "J+ORD_ORI", jumping_wrong_ord_ori
+report = []
 
+try:
+    tool = sys.argv[1].split("/")[1]
+except Exception:
+    tool = "TOOL"
+report.append(tool)
+
+# put info about species, sim, len
+try:
+    species, sim, length, suf = sys.argv[2].split("/")[-1].split(".")
+except Exception:
+    species, sim, length, suf = "A", "B", "C", "D"
+report.extend([species, sim, length])
+
+#print "COPY:" , wrong_copy
+#print "REF:", wrong_ref
+#print "GAP:", wrong_distance
+#print "JUMPING:", jumping
+#print "ORDER_ORIENTATION:", wrong_ord_ori
+#print "J+ORD_ORI", jumping_wrong_ord_ori
+
+report.extend([wrong_copy, wrong_ref, wrong_distance, jumping, wrong_ord_ori, jumping_wrong_ord_ori])
 
 corlens = map(len, corrected_chains)
-print "MEAN CHAIN LENGTH:", sum(corlens) * 1.0 / len(corlens)
-print "MAX CHAIN LENGTH:", max(corlens)
+#print "MEAN CHAIN LENGTH:", sum(corlens) * 1.0 / len(corlens)
+#print "MAX CHAIN LENGTH:", max(corlens)
 
+report.extend([float("%.2f" % (sum(corlens) * 1.0 / len(corlens))), max(corlens)])
 
 # compute n50 (contig chains)
 sumalllens = sum(corlens) * 1.0 / 2
@@ -522,14 +674,19 @@ while (cursum < sumalllens):
     cursum += scorlens[i]
     i += 1
 
-print "CHAIN N50:", scorlens[i - 1]
-
+#print "CORRECTED CHAIN N50:", scorlens[i - 1]
+report.append(scorlens[i - 1])
+report.append(corn50)
 
 correct_links = int(cpx.solution.get_objective_value())
 
-print "CORRECT LINKS:", correct_links
+#print "CORRECT LINKS:", correct_links
+report.append(correct_links)
+
+
 
 inferred_links =  sum(map(lambda x: x - 1, map(len, scaf_answer)))
+report.append(inferred_links)
 
 ref_links = sum(map(lambda x: x - 1, map(len, reference)))
 
@@ -540,4 +697,40 @@ if inferred_links > 0:
     ppv = correct_links * 1.0 / inferred_links
 else:
     ppv = 100.0
-print "%.2f" % sens, "%.2f" % ppv
+#print "%.2f" % sens, "%.2f" % ppv
+
+report.extend([float("%.2f" % sens), float("%.2f" % ppv)])
+
+
+print "TOTAL LINKS", ref_links
+print "CORRECT LINKS", correct_links
+print "INFERRED LINKS", inferred_links
+
+
+sens = float("%.2f" % sens)
+ppv = float("%.2f" % ppv)
+
+if sens == 0 or ppv == 0:
+    fscore = 0
+else:
+    fscore = 2 * sens * ppv / (sens + ppv)
+report.append(float("%.2f" % fscore))
+
+report.append("%.2f" % elapsed)
+print ",".join(map(str, report))
+
+
+"""
+COPY: 0
+REF: 1
+GAP: 0
+JUMPING: 0
+ORDER_ORIENTATION: 0
+J+ORD_ORI 47
+MEAN CHAIN LENGTH: 1.18367346939
+MAX CHAIN LENGTH: 7
+CHAIN N50: 1
+CORRECT LINKS: 108
+0.13 0.69
+"""
+
